44d298fd734a67babc39a799d47caab9a707c294
cajaffem <christian.a.jaffe@gmail.com>
2020-05-29 23:04:38 -0400

    added filter function

--- static/js/app.js
+++ static/js/app.js
@@ -15,5 +15,27 @@ function buildTable(ufos) {
   });
 }
 
+function handleClick() {
+  // filter
+  // buildTable(filterdata)
+}
 
-buildTable(tableData)
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function () {
+  // Select the input element and get the raw HTML node
+  var dateSpace = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputDate = dateSpace.property("value");
+
+  console.log(inputDate);
+  console.log(data);
+
+  var filteredData = data.filter((date) => date.datetime === inputDate);
+
+  buildTable(filteredData);
+});
+
+buildTable(tableData);

39042d676f81b2ff80ad415eb5676f4c0b1bffa0
cajaffem <christian.a.jaffe@gmail.com>
2020-05-30 00:44:11 -0400

    added reset button & 'enter' key functionality

--- static/js/app.js
+++ static/js/app.js
@@ -15,27 +15,31 @@ function buildTable(ufos) {
   });
 }
 
-function handleClick() {
-  // filter
-  // buildTable(filterdata)
-}
-
-// Select the button
 var button = d3.select("#filter-btn");
 
 button.on("click", function () {
-  // Select the input element and get the raw HTML node
   var dateSpace = d3.select("#datetime");
-
-  // Get the value property of the input element
   var inputDate = dateSpace.property("value");
-
-  console.log(inputDate);
-  console.log(data);
-
   var filteredData = data.filter((date) => date.datetime === inputDate);
-
   buildTable(filteredData);
+});
+
+// var inputBox = d3.select("#datetime");
+
+var inputBox = document.getElementById("datetime");
+
+inputBox.addEventListener("keydown", function (event) {
+  if (event.which === 13) {
+    event.preventDefault();
+    document.getElementById("filter-btn").click();
+  }
+});
+
+var reset = d3.select("#reset-btn");
+
+reset.on("click", function () {
+  tbody.html("");
+  buildTable(tableData);
 });
 
 buildTable(tableData);
